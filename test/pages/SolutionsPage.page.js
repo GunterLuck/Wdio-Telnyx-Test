31f063e26cac0f549ec01525a9a94d874c36e8b0
GunterLuck <nkmts2510@gmail.com>
2023-07-13 17:23:33 +0300

    commit

--- test/pages/SolutionsPage.page.js
+++ test/pages/SolutionsPage.page.js
@@ -5,6 +5,7 @@ const accountNotificationWindow = 'ul>li:first-child>div'
 
 class SolutionsPage {
     async clickSeeIndustriesButton() {
+        await $(seeIndustriesButton).waitForClickable()
         await $(seeIndustriesButton).click()
     }
     async scrollToSeeIndustriesButton() {
@@ -14,9 +15,11 @@ class SolutionsPage {
         await $(seeUseCasesButton).click()
     }
     async checkHealthcareButtonVisibility() {
+        await $(healthcareButton).waitForDisplayed(20000)
         await expect (await $(healthcareButton)).toBeDisplayed() == true
     }
     async checkAccountNotificationWindowVisibility() {
+        await $(accountNotificationWindow).waitForDisplayed(20000)
         await expect (await $(accountNotificationWindow)).toBeDisplayed() == true
     }
 }
