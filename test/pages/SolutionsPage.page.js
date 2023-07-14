3194330f88ac012cf7bf2c97f96612074e126f5a
GunterLuck <nkmts2510@gmail.com>
2023-07-14 12:45:27 +0300

    FIX IT

--- test/pages/SolutionsPage.page.js
+++ test/pages/SolutionsPage.page.js
@@ -1,17 +1,19 @@
-const seeIndustriesButton = '[href="#industries"]>span'
-const seeUseCasesButton = '[href="#use-cases"]>span'
-const healthcareButton = 'a[href="/solutions/healthcare"]'
-const accountNotificationWindow = 'ul>li:first-child>div'
+const seeIndustriesButton = 'a[href="#industries"]>span>span'
+const seeUseCasesButton = 'a[href="#use-cases"]>span>span'
+const healthcareButton = 'a[href="/solutions/healthcare"]>div>div:nth-child(2)'
+const accountNotificationWindow = 'div>ul>li:first-child>div>div~h3'
 
 class SolutionsPage {
     async clickSeeIndustriesButton() {
-        await $(seeIndustriesButton).waitForClickable()
-        await $(seeIndustriesButton).click()
+        await $(seeIndustriesButton).waitForClickable(20000)
+        browser.execute('arguments[0].click();', $(seeIndustriesButton));
     }
     async scrollToSeeIndustriesButton() {
+        await $(seeIndustriesButton).waitForDisplayed(20000)
         await $(seeIndustriesButton).scrollIntoView()
     }
     async clickSeeUseCasesButton() {
+        await $(seeUseCasesButton).waitForDisplayed(20000)
         await $(seeUseCasesButton).click()
     }
     async checkHealthcareButtonVisibility() {
